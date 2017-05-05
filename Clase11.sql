a92634ff60e07576ed3dcd2370112551fc0b7a31
dantemartinez <dantemartinez166@gmail.com>
2017-05-05 10:32:40 -0300

    Correccion Ejercicios

--- Clase11.sql
+++ Clase11.sql
@@ -5,6 +5,14 @@ SELECT country, (SELECT COUNT(city)
 FROM country
 ORDER BY country.country_id
 
+# Or
+
+SELECT country, COUNT(*) total
+FROM country
+JOIN city
+USING (country_id)
+GROUP BY country
+
 
 #Ejercicio 2
 SELECT country, COUNT(*) total
@@ -17,20 +25,22 @@ ORDER BY total
 
 
 #Ejercicio 3
-SELECT c.first_name, c.last_name, address, COUNT(rental.rental_id) AS peliculas, SUM(amount) AS total
-FROM customer c
+SELECT first_name, last_name, address, COUNT(rental.rental_id) AS peliculas, SUM(amount) AS total
+FROM customer 
 JOIN address
 	USING (address_id)
 JOIN rental
 	USING (customer_id)
 JOIN payment
-	USING (rental_id)
+	USING (rental_id)   
 GROUP BY 1, 2, 3
 ORDER BY total DESC
 
 #Ejercicio 4
-SELECT DISTINCT title
-FROM film
-LEFT OUTER JOIN inventory
-	USING (film_id)
 
+SELECT DISTINCT film_id, title
+FROM film 
+WHERE film_id NOT IN (SELECT film_id
+                   FROM inventory
+                   WHERE film.film_id = inventory.film_id);
+	
